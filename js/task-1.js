d76b63021c8e74e8ed46117fd20caac4dc46c4b8
Anriy <andrijmilevskij@gmail.com>
2026-05-02 18:07:52 +0300

    add new

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,16 @@
 const btnElem = document.querySelector(".btn");
+const btnElem2 = document.querySelector(".btn-name");
 
 btnElem.addEventListener("click", (e) => {
   const instance = basicLightbox.create(`
     <img src="./images/Adgara.jpeg" width="800" height="1000">
+  `);
+  instance.show();
+});
+
+btnElem2.addEventListener("click", (e) => {
+  const instance = basicLightbox.create(`
+    <img src="./images/Adgara2.jpg" width="800" height="1000">
   `);
   instance.show();
 });
